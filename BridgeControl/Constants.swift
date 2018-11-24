8cc3892db479d77a55f7bdc3cff8eb2f0bd56e4b
Collin Scott <mrmediadesigns@gmail.com>
2018-11-23 22:48:39 -0500

    Completed BridgeControl Tutorial. Code provided was beyond repairable. Had to comment out broken code. Spent hours trying to fix.

---
+++ BridgeControl/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  BridgeControl
+//
+//  Created by Collin Scott on 11/23/18.
+//  Copyright © 2018 collinscott. All rights reserved.
+//
+
+import Foundation
+
+let officerKey = "officer"
+let authorizationCodeKey = "authorizationCode"
+let rankKey = "rank"
+let warpDriveKey = "warp"
+let warpFactorKey = "warpFactor"
+let favoriteTeaKey = "favoriteTea"
+let favoriteCaptainKey = "favoriteCaptain"
+let favoriteGadgetKey = "favoriteGadget"
+let favoriteAlienKey = "favoriteAlien"
