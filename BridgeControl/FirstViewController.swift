3370e27df6fcf8693a692f2fb27a5e4bae66f3ce
Collin Scott <mrmediadesigns@gmail.com>
2018-11-23 20:20:34 -0500

    Initial Commit

---
+++ BridgeControl/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BridgeControl
+//
+//  Created by Collin Scott on 11/23/18.
+//  Copyright © 2018 collinscott. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BridgeControl/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BridgeControl
+//
+//  Created by Collin Scott on 11/23/18.
+//  Copyright © 2018 collinscott. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
